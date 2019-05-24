71a444292e3d280bf1d54c75c4ca92ac54fd3230
Jerry Kuo <jcdkuo@gmail.com>
2019-05-24 16:59:09 +0800

    revise build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
+PROJECT_NAME="drmclient"
 BIN_PATH="./bin"
 DRM_FOR_MAC="drmclient_mac"
 DRM_FOR_LINUX="drmclient_linux"
 DRM_FOR_WINDOWS="drmclient.exe"
-DRM_FOR_ARMLINIX="drmclient_armlinux"
+DRM_FOR_ARMLINUX="drmclient_armlinux"
 
 
 rm -f ${BIN_PATH}/*
 
 # for ARM-Linux
 env GOOS=linux GOARCH=arm GOARM=7 go build
-mv drmclient ${BIN_PATH}/drmclient_armlinux
+mv ${PROJECT_NAME} ${BIN_PATH}/${DRM_FOR_ARMLINUX}
 
 # for Linux
 env GOOS=linux GOARCH=amd64 go build
-mv drmclient ${BIN_PATH}/drmclient_linux
+mv ${PROJECT_NAME} ${BIN_PATH}/${DRM_FOR_LINUX}
 
 # for Windows
 env GOOS=windows GOARCH=amd64 go build
-mv drmclient.exe ${BIN_PATH}/
+mv ${PROJECT_NAME}.exe ${BIN_PATH}/${DRM_FOR_WINDOWS}
 
 # for MacOS
 env GOOS=darwin GOARCH=amd64 go build
-mv drmclient ${BIN_PATH}/drmclient_mac
+mv ${PROJECT_NAME} ${BIN_PATH}/${DRM_FOR_MAC}
 
