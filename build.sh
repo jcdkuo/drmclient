6c88143fb3e26aff0ff886e7a4f249ce4ad56df2
Jerry Kuo <jcdkuo@gmail.com>
2019-06-12 19:20:43 +0800

    [M]revise color setting in build.sh

--- build.sh
+++ build.sh
@@ -7,14 +7,19 @@ DRM_FOR_LINUX="drmclient_linux"
 DRM_FOR_WINDOWS="drmclient.exe"
 DRM_FOR_ARMLINUX="drmclient_armlinux"
 
+COLOR_RESET='\033[0m'
+COLOR_RED='\033[1;31m'
+COLOR_WHITE='\033[1;37m'
+COLOR_B_BLUE='\033[1;44m'
+
 START_MSG()
 {
-    echo "\033[1;31m Starting to build ${PROJECT_NAME} for ${1}...... \033[0m"
+    echo "${COLOR_RED} Starting to build ${PROJECT_NAME} for ${1}...... ${COLOR_RESET}"
 }
 
 END_MSG()
 {
-    echo "\033[1;37m -> \033[1;44m ${1} \033[0m \033[1;37m is now built in bin/ \033[0m"
+    echo "${COLOR_WHITE} -> ${COLOR_B_BLUE} ${1} ${COLOR_RESET} ${COLOR_WHITE} is now built in bin/ ${COLOR_RESET}"
 }
 
 rm -f ${BIN_PATH}/*
